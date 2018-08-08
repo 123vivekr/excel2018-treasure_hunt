365b2ec17eece1437214739683a902f938a78612
Aswin M Prabhu <aswinmprabhu@gmail.com>
2018-08-08 22:41:45 +0530

    add google login button

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import Sidebar from './Sidebar';
 import '../css/Main.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import GoogleButton from 'react-google-button'
 
 class Main extends Component {
   constructor(props) {
@@ -24,43 +25,9 @@ class Main extends Component {
   authenticate = () => {
     return(
       <div id="logincard">
-        <div id="logodiv">
-          <br />
-          <br />
-          <br />
-          <br />
-          <img id='logo' src="http://excelmec.org/partners/img/excel2018.14bdbf062ab3f85e249a4d31cd4f0584.png"/>
-        </div>
-        <br />
-        <br />
-        <div id="authentication">
-          <TextField
-            id="email"
-            label="Email"
-            className='email'
-            // value={'Email'}
-            onChange={this.handleChange('email')}
-            fullWidth
-            margin="normal"
-          />
-          <br />
-          <TextField
-            id="pass"
-            label="Passowrd"
-            // value={this.state.name}
-            onChange={this.handleChange('pass')}
-            fullWidth
-            margin="normal"
-          />
-          <br />
-          <br />
-          <br />
-          <center>
-          <Button color="primary"  variant="contained">
-            Log In
-          </Button> 
-          </center>
-        </div>
+        <GoogleButton
+          onClick={() => { console.log('Google button clicked') }}
+        />
       </div>
     );
   }
