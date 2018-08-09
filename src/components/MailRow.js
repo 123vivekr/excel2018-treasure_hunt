68155ae1e056c273b5cf08d430c81f3170d3b9da
Aswin G <aswinganesh666@gmail.com>
2018-08-09 22:08:12 +0530

    Migrated inline styles from MailTemplate to MailStyles

--- src/components/MailRow.js
+++ src/components/MailRow.js
@@ -1,29 +1,5 @@
 import React, { Component } from "react";
-import Sidebar from "./Sidebar";
 import "../css/MailStyles.css";
-
-const styles = {
-  mailRow: {
-    backgroundColor: "rgba(0,0,0,0.87)",
-    height: "50px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    color: "rgba(255,255,255,0.87)",
-    padding: "0px 24px 0px 24px",
-    margin: "4px",
-    borderRadius: "15px",
-    cursor: "pointer"
-  },
-  title: {
-    color: "yellow",
-    overflow: "ellipsis"
-  },
-  timestamp: {
-    fontWeight: 700,
-    overflow: "ellipsis"
-  }
-};
 
 class MailRow extends Component {
   constructor(props) {
--- src/components/MailTemplate.js
+++ src/components/MailTemplate.js
@@ -1,39 +1,5 @@
 import React, { Component } from "react";
-import Sidebar from "./Sidebar";
 import "../css/MailStyles.css";
-
-const styles = {
-  mailDiv: {
-    backgroundColor: "rgba(0,0,0,0.87)",
-    height: "50px",
-    display: "flex",
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    color: "rgba(255,255,255,0.87)",
-    paddingLeft: "24px",
-    paddingRight: "24px",
-    margin: "4px",
-    borderRadius: "15px"
-  },
-  title: {
-    color: "yellow",
-    overflow: "ellipsis",
-    padding: "4px",
-    paddingTop: "16px"
-  },
-  timestamp: {
-    fontWeight: 700,
-    padding: "4px"
-  },
-  content: {
-    flex: 1,
-    padding: "4px"
-  },
-  fullWidth: {
-    width: "100%"
-  }
-};
 
 class MailTemplate extends Component {
   constructor(props) {
@@ -44,11 +10,11 @@ class MailTemplate extends Component {
   render() {
     const { title, timestamp, content } = this.props;
     return (
-      <div style={styles.mailDiv}>
-        <span style={styles.title}>{title}</span>
-        <span style={styles.timestamp}>{timestamp}</span>
-        <hr style={styles.fullWidth} />
-        <span style={styles.content}>{content}</span>
+      <div className="mailDiv">
+        <span className="expandedtitle">{title}</span>
+        <span className="expandedtimestamp">{timestamp}</span>
+        <hr className="fullWidth" />
+        <span className="content">{content}</span>
       </div>
     );
   }
