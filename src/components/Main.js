377bb558fb061616a924e7b062ae7432db0b40c0
agzuniverse <aswinganesh666@gmail.com>
2018-08-09 11:48:41 +0530

    Added mail row component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,62 +1,91 @@
-import React, { Component } from 'react';
-import Sidebar from './Sidebar';
-import '../css/Main.css';
-import '../css/Sidebar.css';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import GoogleButton from 'react-google-button'
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import "../css/Main.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import GoogleButton from "react-google-button";
+import MailRow from "./MailRow";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       authStatus: false,
-      name: '',
-      email: '',
+      name: "",
+      email: "",
+      mailList: [
+        {
+          mailHeader: {
+            title: "Dummy challenge",
+            timestamp: "2018-8-16 2:30PM"
+          },
+          mailBody: {
+            content: "lorem ipsum"
+          }
+        },
+        {
+          mailHeader: {
+            title: "Another one",
+            timestamp: "2018-9-1 11:30PM"
+          },
+          mailBody: {
+            content: "dolor sit amet"
+          }
+        }
+      ]
     };
   }
 
   handleChange = name => event => {
     this.setState({
-      [name]: event.target.value,
+      [name]: event.target.value
     });
   };
- 
+
   challenges = () => {
-    return(
-      <div id="challengecard">
-      </div>   
-    )
-   
-  }
+    const mails = this.state.mailList.map(mail => {
+      return (
+        <MailRow
+          title={mail.mailHeader.title}
+          timestamp={mail.mailHeader.timestamp}
+        />
+      );
+    });
+
+    return <div id="challengecard">{mails}</div>;
+  };
 
   authenticate = () => {
-    return(
+    return (
       <div id="logincard">
-      <div className="design1 design"></div>
-      <div className="design2 design"></div>
+        <div className="design1 design" />
+        <div className="design2 design" />
         <div id="logodiv">
-          <img id='logo' src="http://excelmec.org/partners/img/excel2018.14bdbf062ab3f85e249a4d31cd4f0584.png"/>
+          <img
+            id="logo"
+            src="http://excelmec.org/partners/img/excel2018.14bdbf062ab3f85e249a4d31cd4f0584.png"
+          />
         </div>
         <div className="btn">
-        <GoogleButton
-          onClick={() => { console.log('Google button clicked') }}
-        />
+          <GoogleButton
+            onClick={() => {
+              console.log("Google button clicked");
+            }}
+          />
         </div>
       </div>
     );
-  }
+  };
 
   render() {
-    
     return (
       <div className="Login">
         <div className="sidebar">
-          <Sidebar page={ "login" } />
+          <Sidebar page={"login"} />
         </div>
         <div className="mainbox">
-          {/* { this.challenges() } */}
-          {this.authenticate()}
+          {this.challenges()}
+          {/* {this.authenticate()} */}
         </div>
       </div>
     );
