15967234dc9253f5bf05eae9356769f08735f0dd
agzuniverse <aswinganesh666@gmail.com>
2018-08-09 14:43:37 +0530

    Fixed zero height sidebar bug

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import '../css/Sidebar.css';
+import React, { Component } from "react";
+import "../css/Sidebar.css";
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="sidebar">
-        <div className="sherlock">
-          
-        </div>
+        <div className="sherlock" />
       </div>
     );
   }
