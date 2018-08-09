34699bfb784e5fadc235a5ee8997905ad472554a
agzuniverse <aswinganesh666@gmail.com>
2018-08-09 13:20:02 +0530

    Created Mail Template

---
+++ src/components/MailTemplate.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import "../css/MailStyles.css";
+
+const styles = {
+  mailRow: {
+    width: "100%",
+    backgroundColor: "rgba(0,0,0,0.87)",
+    height: "50px",
+    display: "flex",
+    alignItems: "center",
+    color: "rgba(255,255,255,0.87)",
+    paddingLeft: "24px",
+    margin: "4px",
+    borderRadius: "15px"
+  },
+  title: {
+    color: "yellow",
+    overflow: "ellipsis"
+  },
+  timestamp: {
+    fontWeight: 700
+  }
+};
+
+class MailTemplate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { title, timestamp } = this.props;
+    return (
+      <div style={styles.mailRow}>
+        <span style={styles.title}>{title}</span>
+        <span style={styles.timestamp}>{timestamp}</span>
+      </div>
+    );
+  }
+}
+export default MailTemplate;
